10657c4b068425419192fbd1d4fa2a685703442e
Danish Mehmood <danishmehmood981@gmail.com>
2023-01-22 16:50:29 +0500

    another commit

--- src/com/dataStructures/Main.java
+++ src/com/dataStructures/Main.java
@@ -1,12 +1,17 @@
 package com.dataStructures;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
-
-
+        // write your code here
 
 
     }
+
+
 }
+
